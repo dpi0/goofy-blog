88dfac77852daf9f4ed777cfc51f17bc11d0490a
dpi0 <dpi0.dev@proton.me>
2024-07-24 23:20:49 +0530

    feat: add scratch dark mode button

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,30 @@
+function initializeThemeToggle() {
+  document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.getElementById("theme-toggle");
+    const currentTheme = localStorage.getItem("theme") || "light";
+
+    document.documentElement.setAttribute("data-theme", currentTheme);
+    updateIcon(currentTheme);
+
+    toggleButton.addEventListener("click", function () {
+      const currentTheme = document.documentElement.getAttribute("data-theme");
+      const newTheme = currentTheme === "light" ? "dark" : "light";
+      document.documentElement.setAttribute("data-theme", newTheme);
+      localStorage.setItem("theme", newTheme);
+      updateIcon(newTheme);
+    });
+  });
+
+  function updateIcon(theme) {
+    const toggleButton = document.getElementById("theme-toggle");
+    const moonIcon = toggleButton.getAttribute("data-moon-icon");
+    const sunIcon = toggleButton.getAttribute("data-sun-icon");
+    toggleButton.innerHTML = theme === "light" ? moonIcon : sunIcon;
+    toggleButton.title =
+      theme === "light" ? "Switch to Dark mode" : "Switch to Light mode";
+  }
+}
+
 function enableNavBarDropdown() {
   document.addEventListener("DOMContentLoaded", function () {
     const sidebar = document.getElementById("sidebar");
@@ -11,4 +38,5 @@ function enableNavBarDropdown() {
 
 //--------------------------------------------
 
+initializeThemeToggle();
 enableNavBarDropdown();
